112a13329ac2c8248cc96d26bfeccdf57ead8b8a
Rolf Simoes <rolf.simoes@opengeohub.org>
2023-12-15 13:23:11 +0100

    Update example

--- inst/sits/example.R
+++ inst/sits/example.R
@@ -11,7 +11,12 @@ processes_file <- system.file("sits/processes.R", package = "openeocraft")
 load_processes(api, processes_file)
 
 catalog_file <- system.file("sits/catalog.json", package = "openeocraft")
-load_collections(api)
+stac_endpoint <- "https://example.com/stac"
+load_collections(api, catalog = catalog_file)
+load_collections(api, stac = stac_endpoint)
+
+
+load_collections(api, function() {})
 
 run_api(api, host = "127.0.0.1", port = 8001)
 
