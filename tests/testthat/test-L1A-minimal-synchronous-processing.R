948b02d69561fe2fbb9fa5303b9c648de9475dd5
Rolf Simoes <rolf.simoes@opengeohub.org>
2025-01-10 17:58:21 +0100

    Add testing integration

--- tests/testthat/test-L1A-minimal-synchronous-processing.R
+++ tests/testthat/test-L1A-minimal-synchronous-processing.R
@@ -5,36 +5,17 @@
 # POST /result > Content-Type header	Returns suitable media type for the requested file format (not always application/octet-stream) in the Content-Type header
 # POST /result	Rejects processing if a payment is required.
 
-# Create an STAC server API object
-stac_api <- openstac::create_stac(
-  id = "openlandmap",
-  title = "OpenLandMap STAC API",
-  description = paste(
-    "Spatio-Temporal Asset Catalog for global layers provided ",
-    "by OpenLandMap and maintaned by OpenGeoHub Foundation"
-  ),
-  conforms_to = NULL
-)
-
-# Set API database
-file <- system.file("ml/db.rds", package = "openeocraft")
-stac_api <- openstac::set_db(stac_api, driver = "local", file = file)
-
 # Create openEO API object
 api <- create_openeo_v1(
   id = "openeocraft",
   title = "openEO compliant R backend",
   description = "OpenEOcraft offers a robust R framework designed for the development and deployment of openEO API applications.",
   backend_version = "0.2.0",
-  stac_api = stac_api,
-  work_dir = "~/openeo-tests",
+  stac_api = NULL,
+  work_dir = getwd(),
   conforms_to = NULL,
   production = FALSE
 )
-
-# Load processes
-processes_file <- system.file("ml/processes.R", package = "openeocraft")
-load_processes(api, processes_file)
 
 test_that("POST /result	Is supported (with authentication)", {
   # Call without token
